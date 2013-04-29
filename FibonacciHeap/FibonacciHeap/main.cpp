b8c8ee5a1f8a706c5d243acc03aacc2096aeed69
David Thacker <davidt@alooma.com>
2013-04-28 22:14:32 -0500

    Initial Commit

---
+++ FibonacciHeap/FibonacciHeap/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  FibonacciHeap
+//
+//  Created by David Thacker on 4/28/13.
+//  Copyright (c) 2013 David Argyle Thacker. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
